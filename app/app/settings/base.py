c71e2ce4399c6f411aa911562b5faf4e79779193
Natalyn Middleton-Yu <natalyn.yu@gmail.com>
2021-08-11 14:34:09 -0400

    Rename FeedPeople app to FeedPerson
    The app should be named similarly to the primary model (FeedPerson).

--- app/app/settings/base.py
+++ app/app/settings/base.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'feedpeople',
+    'feedperson',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- app/feedperson/apps.py
+++ app/feedperson/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class FeedpeopleConfig(AppConfig):
+class FeedpersonConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'feedpeople'
+    name = 'feedperson'
