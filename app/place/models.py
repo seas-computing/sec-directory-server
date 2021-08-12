3fe72703b9057b9393a252308671f482a18a81aa
Natalyn Middleton-Yu <natalyn.yu@gmail.com>
2021-08-12 13:23:44 -0400

    Remove blank and null properties from name and location
    The blank and null properties default to False.

--- app/place/models.py
+++ app/place/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 # Create your models here.
 class Place(models.Model):
-  name = models.CharField('Place name', max_length=255, blank=False, null=False)
-  location = models.CharField('Location', max_length=255, blank=False, null=False)
+  name = models.CharField('Place name', max_length=255)
+  location = models.CharField('Location', max_length=255)
 
   def __str__(self):
     return self.name
