9999459dc63ba0f7912af2672f0689e11fe80e2e
Natalyn Middleton-Yu <natalyn.yu@gmail.com>
2021-08-24 13:51:28 -0400

    Update index file to only allow searching by the 'name' field
    https://www.algolia.com/doc/framework-integration/django/setup/?client=python

--- app/rollup/index.py
+++ app/rollup/index.py
@@ -1,5 +1,9 @@
 import algoliasearch_django as algoliasearch
-
+from algoliasearch_django.decorators import register
 from .models import Rollup
+from os import environ
 
-algoliasearch.register(Rollup)
+@register(Rollup)
+class RollupIndex(algoliasearch.AlgoliaIndex):
+  settings = {'searchableAttributes': ['name']}
+  index_name = environ.get("ALGOLIA_INDEX")
