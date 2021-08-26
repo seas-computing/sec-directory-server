8e31ba7f7c927c0074a899ae400a3b85f21e55d6
Chris Rogers <chrxr@outlook.com>
2021-08-26 11:07:24 -0400

    Implemented a clear-index function to be used with load_rollup. This prevents duplicate records being created by forcing the index to be cleared before pushing the records to Algolia.

--- app/rollup/utils.py
+++ app/rollup/utils.py
@@ -1,10 +1,21 @@
+from os import environ
 from .models import Rollup
 from place.models import Place
 from person.models import Person
 from feedperson.models import FeedPerson
+from algoliasearch.search_client import SearchClient
+
+# Function to force a clear out of the index before reindexing.
+# Prevents duplicate records being created if updates are pushed from two different DBs.
+# This could happen if the production machine / DB are rebuilt.
+def clear_index():
+  client = SearchClient.create(environ.get("ALGOLIA_APP_ID"), environ.get("ALGOLIA_API_KEY"))
+  index = client.init_index(environ.get("ALGOLIA_INDEX"))
+  index.clear_objects()
 
 # Creates updated Rollup objects using Place, Person, and FeedPerson data
 def load_rollup():
+  clear_index()
   Rollup.objects.all().delete()
   places = Place.objects.all()
   people = Person.objects.all()
@@ -16,3 +27,6 @@ def load_rollup():
         name = object.name,
         location = object.location
       )
+
+
+
