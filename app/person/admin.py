0e4476f55beccb79f1984361a366e720fd19b8ef
Natalyn Middleton-Yu <natalyn.yu@gmail.com>
2021-08-26 19:21:19 -0400

    Add Update Screens button to Place/Person Django Admin pages
    The "Update Screens" buttons call the load_rollup function.

--- app/person/admin.py
+++ app/person/admin.py
@@ -1,5 +1,18 @@
-from django.contrib import admin
-
+from django.contrib import admin, messages
+from rollup.utils import load_rollup
+from django_object_actions import DjangoObjectActions
 from .models import Person
-# Register your models here.
-admin.site.register(Person)
+
+# Adds a button to the Person Admin page to allow users to manually update the
+# directory screens with the most up-to-date data
+class PersonAdmin(DjangoObjectActions, admin.ModelAdmin):
+
+  def UpdateScreens(self, request, queryset):
+    load_rollup()
+    self.message_user(request, 'Successfully updated screens with revised person and place data.', level=messages.SUCCESS, fail_silently=False)
+  
+  UpdateScreens.label = 'Update screens'
+        
+  changelist_actions = ('UpdateScreens', )
+
+admin.site.register(Person, PersonAdmin)
--- app/place/admin.py
+++ app/place/admin.py
@@ -1,6 +1,18 @@
-from django.contrib import admin
-
+from django.contrib import admin, messages
+from rollup.utils import load_rollup
+from django_object_actions import DjangoObjectActions
 from .models import Place
 
-# Register your models here.
-admin.site.register(Place)
+# Adds a button to the Person Admin page to allow users to manually update the
+# directory screens with the most up-to-date data
+class PlaceAdmin(DjangoObjectActions, admin.ModelAdmin):
+
+  def UpdateScreens(self, request, queryset):
+    load_rollup()
+    self.message_user(request, 'Successfully updated screens with revised person and place data.', level=messages.SUCCESS, fail_silently=False)
+  
+  UpdateScreens.label = 'Update screens'
+        
+  changelist_actions = ('UpdateScreens', )
+
+admin.site.register(Place, PlaceAdmin)
