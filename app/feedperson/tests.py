939982d1c5f6b0345734c6f2343a41a1393a29f8
Chris Rogers <chrxr@outlook.com>
2021-08-31 20:36:33 -0400

    Added a couple of regexes to fix locations on import. Whilst doing the tests, I moved things around to reduce the test time from 30 seconds to 15.

--- app/feedperson/tests.py
+++ app/feedperson/tests.py
@@ -14,16 +14,29 @@ class FeedPersonTestCase(TestCase):
     feed_person = FeedPerson.objects.get(id=1)
     self.assertEqual(feed_person.name, str(feed_person))
 
-  def test_load_feed_people_removes_original_data(self):
-    feed_person = FeedPerson.objects.get(id=1)
+class LoadFeedTest(TestCase):
+  
+  @classmethod
+  def setUpTestData(cls):
     with disable_auto_indexing():
       load_feed_people()
-    self.assertEqual(FeedPerson.objects.filter(eppn=feed_person.eppn).count(), 0)
 
   def test_load_feed_people_imports_western_ave_people(self):
-    with disable_auto_indexing():
-      load_feed_people()
     western_ave_people = filter(lambda person: "Western Ave" in person.location, FeedPerson.objects.all())
     non_western_ave_people = filter(lambda person: "Western Ave" not in person.location, FeedPerson.objects.all())
     self.assertTrue(len(list(western_ave_people)) > 0)
     self.assertEqual(len(list(non_western_ave_people)), 0)
+
+  def test_load_feed_people_regex_removes_all_SciEnG(self):
+    scieng_people = filter(lambda person: "Sci&Eng" in person.location, FeedPerson.objects.all())
+    self.assertTrue(len(list(scieng_people)) == 0)
+
+  def test_load_feed_people_regex_removes_all_western_avenue(self):
+    western_avenue_people = filter(lambda person: "Western Avenue" in person.location, FeedPerson.objects.all())
+    self.assertTrue(len(list(western_avenue_people)) == 0)
+
+  def test_load_feed_people_removes_original_data(self):
+    feed_person = FeedPerson.objects.create(eppn="1a2b3c456def7890", firstname="John", lastname="Harvard", location="Pierce Hall 101", name="John Harvard")
+    with disable_auto_indexing():
+      load_feed_people()
+    self.assertEqual(FeedPerson.objects.filter(eppn=feed_person.eppn).count(), 0)
--- app/feedperson/utils.py
+++ app/feedperson/utils.py
@@ -19,6 +19,13 @@ def load_feed_people():
   for person in people:
     location = person.location.fordisp.text
     if (re.match('114 Western Ave', location) or re.match('150 Western Ave', location)):
+      
+      # This regex is a temp fix whilst the location data is inconsistent in PeopleSoft.
+      # Should be removed once the data is fixed.
+      if re.match('150 Western Ave', location):
+        location = re.sub(r'Sci&Eng', 'SEC', location)
+        location = re.sub(r'150 Western Avenue', '150 Western Ave, SEC', location)
+      
       FeedPerson.objects.create(
         eppn=person.eppn.text,
         firstname=person.givenname.text,
