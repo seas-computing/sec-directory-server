8033dc662a073481c794c2ac91184f7786e124c3
Jonathan Seitz <jseitz@seas.harvard.edu>
2021-09-02 09:22:19 -0400

    Add exec option
    This changes from a boolean PRODUCTION value to an EXEC_MODE value that
    can be set to production or development, but defaults to "exec". In exec
    mode, it will run whatever is passed in as the command value so that we
    can run the load_feed_people() function or other django-admin commands.
    In the other modes, it will run the app in either production or
    development mode as indicated, after running migrations and creating the
    superuser.

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -1,32 +1,36 @@
 #!/bin/sh
 
 # default to development mode
-PRODUCTION=false
+EXEC_MODE=exec
 
-for arg; do
-  if [[ $arg == "--production" ]]; then
-    PRODUCTION=true
-  fi
-done
+if [[ "$*" == "--production" ]]; then
+  EXEC_MODE=production
+elif [[ "$*" == "--development" ]]; then
+  EXEC_MODE=development
+fi
 
 if [ "$DATABASE" = "postgres" ]
 then
-    echo "Waiting for postgres..."
+  echo "Waiting for postgres..."
 
-    while ! nc -z $SQL_HOST $SQL_PORT; do
-      sleep 0.1
-    done
+  while ! nc -z $SQL_HOST $SQL_PORT; do
+    sleep 0.1
+  done
 
-    echo "PostgreSQL started"
+  echo "PostgreSQL started"
 fi
 
-python manage.py migrate
-python manage.py createsuperuser --noinput
-if [[ $PRODUCTION == true ]]; then
-  echo "Starting App in production mode..."
-  python manage.py collectstatic --no-input --clear
-  gunicorn app.wsgi:application --bind 0.0.0.0:8000
+if [[ $EXEC_MODE == exec ]]; then
+  echo "Running $@"
+  exec "$@"
 else
-  echo "Starting app in development mode..."
-  python manage.py runserver 0.0.0.0:8000
+  python manage.py migrate
+  python manage.py createsuperuser --noinput
+  if [[ $EXEC_MODE == development ]]; then
+    echo "Starting app in development mode..."
+    python manage.py runserver 0.0.0.0:8000
+  elif [[ $EXEC_MODE == production ]]; then
+    echo "Starting App in production mode..."
+    gunicorn app.wsgi:application --bind 0.0.0.0:8000
+  fi
 fi
