b22d55c99fc26d9ae9207579375409aa1d200bdd
Natalyn Middleton-Yu <natalyn.yu@gmail.com>
2021-09-08 13:03:31 -0400

    Add tests to check for the update screens error message in the case of an exception
    This test adds a mock exception when load_rollup is called on click of the update screens button, and it checks that the error message is present on the screen.

--- app/person/tests.py
+++ app/person/tests.py
@@ -1,6 +1,8 @@
 from django.test import TestCase, Client
 from django.contrib.auth.models import User
 from algoliasearch_django.decorators import disable_auto_indexing
+import requests
+from unittest.mock import patch
 
 from .models import Person
 
@@ -9,6 +11,7 @@ test_email = 'test@test.com'
 test_password = 'TesterPassword'
 update_screens_button_label = 'Update screens'
 success_message = 'Successfully updated screens with revised person and place data.'
+error_message = 'Error: Unable to update screens. If the problem persists, contact SEAS Computing.'
 c = Client()
 
 # Create your tests here.
@@ -44,15 +47,22 @@ class PersonTestCase(TestCase):
     label = person._meta.get_field('name').verbose_name
     self.assertEqual(label, 'Full name')
   
-  def test_update_screens_button_renders(self):
+  def test_person_update_screens_button_renders(self):
     response = c.get('/admin/person/person/')
   
     self.assertEqual(response.status_code, 200)
     self.assertTrue(update_screens_button_label in response.content.decode('utf-8'))
 
-  def test_click_update_screens_button_result(self):
+  def test_click_person_update_screens_button_successful_result(self):
     with disable_auto_indexing():
-      response = c.get('/admin/person/person/actions/UpdateScreens/', follow=True)
+      response = c.get('/admin/person/person/actions/UpdateScreensButton/', follow=True)
 
     self.assertEqual(response.status_code, 200)
     self.assertTrue(success_message in response.content.decode('utf-8'))
+
+  @patch('rollup.utils.load_rollup')
+  def test_click_person_update_screens_button_error(self, mock_load_rollup):
+    mock_load_rollup.side_effect = Exception('Test error')
+    with disable_auto_indexing():
+      response = c.get('/admin/person/person/actions/UpdateScreensButton/', follow=True)
+    self.assertTrue(error_message in response.content.decode('utf-8'))
--- app/place/tests.py
+++ app/place/tests.py
@@ -1,6 +1,8 @@
 from django.test import TestCase, Client
 from django.contrib.auth.models import User
 from algoliasearch_django.decorators import disable_auto_indexing
+import requests
+from unittest.mock import patch
 
 from .models import Place
 
@@ -9,6 +11,7 @@ test_email = 'test@test.com'
 test_password = 'TesterPassword'
 update_screens_button_label = 'Update screens'
 success_message = 'Successfully updated screens with revised person and place data.'
+error_message = 'Error: Unable to update screens. If the problem persists, contact SEAS Computing.'
 c = Client()
 
 # Create your tests here.
@@ -34,15 +37,22 @@ class PlaceTestCase(TestCase):
     label = place._meta.get_field('location').verbose_name
     self.assertEqual(label, 'Location')
   
-  def test_update_screens_button_renders(self):
+  def test_place_update_screens_button_renders(self):
     response = c.get('/admin/place/place/')
   
     self.assertEqual(response.status_code, 200)
     self.assertTrue(update_screens_button_label in response.content.decode('utf-8'))
 
-  def test_click_update_screens_button_result(self):
+  def test_click_place_update_screens_button_result(self):
     with disable_auto_indexing():
-      response = c.get('/admin/place/place/actions/UpdateScreens/', follow=True)
+      response = c.get('/admin/place/place/actions/UpdateScreensButton/', follow=True)
 
     self.assertEqual(response.status_code, 200)
     self.assertTrue(success_message in response.content.decode('utf-8'))
+  
+  @patch('rollup.utils.load_rollup')
+  def test_click_place_update_screens_button_error(self, mock_load_rollup):
+    mock_load_rollup.side_effect = Exception('Test error')
+    with disable_auto_indexing():
+      response = c.get('/admin/place/place/actions/UpdateScreensButton/', follow=True)
+    self.assertTrue(error_message in response.content.decode('utf-8'))
