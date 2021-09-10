877b5a17e62d293e0159e3cf1b14c612be9c84b7
Chris Rogers <chrxr@outlook.com>
2021-09-10 12:12:06 -0400

    Changed STATIC_URL to take an environment variable so that we can point to S3 for static files

--- app/app/settings/base.py
+++ app/app/settings/base.py
@@ -129,7 +129,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_ROOT = path.join(BASE_DIR, 'static')
-STATIC_URL = '/static/'
+STATIC_URL = environ.get("STATIC_URL", '/static/')
 MEDIA_ROOT = path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
